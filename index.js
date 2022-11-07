198757f9b47a95a5e58d5b2ad29a8e2bd08f8d23
jakebroffman <jake.broffman1088@gmail.com>
2022-11-07 12:27:41 -0500

    commit tree 12d5eb32525ae37b9a0a1d55295057f79b3e9d8b

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name: "Jake Broffman",
+    streetAddress: "131 Vandalia Street, Buffalo, NY, 14204"
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {
+        ...obj,
+        [key]: value,
+    };
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+};
+
+function deleteFromEmployeeByKey(obj, key) {
+let newEmployee ={...obj}
+delete newEmployee[key]
+return newEmployee
+};
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    
+    delete employee[key]
+    return employee
+};
